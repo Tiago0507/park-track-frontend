3b5a8d7d3f1701630e41e4b8a1beceb09fadb36b
EstebanGZam <estebangaviria82@gmail.com>
2024-10-14 21:13:50 -0500

    feat: show test description by querying the information displayed from the backend

--- src/js/init-test.js
+++ src/js/init-test.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const levodopaTimeInput = document.getElementById('levodopaTime');
     const restartTest = document.getElementById('restartTest');
 
+    const testTypeSelect = document.getElementById('testType');
+    const testDescriptionDiv = document.getElementById('testDescriptionDiv');
+    const testDescriptionText = document.getElementById('testDescription');
+
     stateOffRadio.addEventListener('change', function () {
         levodopaTimeDiv.style.display = this.checked ? 'block' : 'none';
         levodopaTimeInput.required = this.checked;
@@ -21,5 +25,29 @@ document.addEventListener('DOMContentLoaded', function () {
         form.reset();
         this.disabled = true;
         levodopaTimeDiv.style.display = 'none';
+    });
+
+    
+    // Manejar el cambio en el selector de tipo de prueba
+    testTypeSelect.addEventListener('change', function () {
+        const selectedTest = testTypeSelect.value;
+
+        // Si se selecciona una prueba, hacer una petición al backend
+        if (selectedTest) {
+            fetch(`http://localhost:8080/api/getTestDescription?testType=${selectedTest}`)
+                .then(response => response.json())
+                .then(data => {
+                    // Mostrar la descripción de la prueba
+                    testDescriptionText.value = data.description;
+                    testDescriptionDiv.style.display = 'block';
+                })
+                .catch(error => {
+                    console.error('Error al obtener la descripción de la prueba:', error);
+                });
+        } else {
+            // Si no se selecciona ninguna prueba, ocultar la descripción
+            testDescriptionDiv.style.display = 'none';
+            testDescriptionText.value = '';
+        }
     });
 });
