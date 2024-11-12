dfcf9a1d3a6b0ab3d5f8dccb20ab0d8992e782d4
EstebanGZam <estebangaviria82@gmail.com>
2024-11-11 22:42:25 -0500

    feat: added evaluator registration

--- src/scripts/register-user.js
+++ src/scripts/register-user.js
@@ -5,6 +5,49 @@ document.getElementById("user-type").addEventListener("change", function () {
 });
 
 function submitForm() {
+    const userType = document.getElementById("user-type").value;
+    const commonData = {
+        id_number: document.getElementById("id_number").value,
+        first_name: document.getElementById("first_name").value,
+        last_name: document.getElementById("last_name").value,
+        email: document.getElementById("email").value
+    };
+
+    let userData;
+
+    if (userType === "evaluator") {
+        userData = {
+            ...commonData,
+            password: document.getElementById("password").value,
+            role: "EVALUATOR"
+        };
+    } else if (userType === "evaluated") {
+        userData = {
+            ...commonData,
+            sex: document.getElementById("gender").value,
+            date_of_birth: document.getElementById("date_of_birth").value,
+            family_history_parkinson: document.getElementById("family_history_parkinson").value,
+            height: document.getElementById("height").value,
+            weight: document.getElementById("weight").value,
+            evaluated_type: document.getElementById("evaluated_type").value
+        };
+    }
+
+    console.log(userData);
+    // Realizar la solicitud de registro
+    fetch(`http://127.0.0.1:8080/${userType === "evaluator" ? "evaluators" : "patients"}/add`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            alert(`${userType === "evaluator" ? "Evaluador" : "Evaluado"} registrado con éxito`);
+            resetForm();
+        })
+        .catch(error => console.error("Error al registrar:", error));
 
 }
 
